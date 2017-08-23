b4e3aa26e12f3d99159a9e50870529d1c61bd8ff
ZhihaoLi <839008347@qq.com>
2017-08-23 10:56:13 +0800

    数组扩容的方法，对象复制的方法

---
+++ ArrayUtil.java
@@ -0,0 +1,50 @@
+﻿package com.ideabook.util;
+
+import java.util.Arrays;
+
+/**
+ * @Author: lzh
+ * @Description:jdk1.8测试可用
+ * @Date: Created in 2017/8/22 16:13
+ */
+public class ArrayUtil {
+
+    /**
+     * 对一维数组进行扩容
+     * @param datas 原始数组
+     * @param newLen 扩容大小(增加多少容量)
+     * @param <T>
+     * @return
+     */
+    public static <T> T[] expandCapacity(T[] datas,int newLen){
+        int oldLen = datas.length;
+        newLen = newLen < 0 ? oldLen : oldLen + newLen;
+        return Arrays.copyOf(datas,newLen);
+    }
+
+    /**
+     * 对数组进行扩容处理，变为1.5倍。
+     * @param datas 原数组
+     * @param <T> 数组的类型
+     * @return T[] 返回数组
+     */
+    public static <T> T[] expandCapacity(T[] datas){
+        int newLen = (datas.length * 3) /2 ;
+        //生成一个新的数组
+        return Arrays.copyOf(datas,newLen);
+    }
+
+    /**
+     * 对数组进行成倍的扩容处理,
+     * @param datas 原数组
+     * @param mulitiple 扩容倍数
+     * @param <T>
+     * @return
+     */
+    public static <T> T[] expandCapacityMlu(T[] datas,int mulitiple){
+        mulitiple = mulitiple < 0 ? 1 : mulitiple;
+        int newLen = datas.length * mulitiple;
+        return Arrays.copyOf(datas,newLen);
+    }
+
+}
---
+++ CloneUtil.java
@@ -0,0 +1,39 @@
+﻿package com.ideabook.util;
+
+import java.io.*;
+
+/**
+ * @Author: lzh
+ * @Description: jdk1.8测试可用
+ * @Date: Created in 2017/8/21 15:23
+ */
+public class CloneUtil {
+    /**
+     * 使用该工具类的对象必须要实现Serializable接口，否则是没有办法实现克隆的。
+     * @param obj 序列化的对象
+     * @param <T> 对象类型
+     * @return 复制的新对象
+     */
+    public static <T extends Serializable> T clone(T obj){
+        T cloneObj = null;
+        try {
+            //写入字节流
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            ObjectOutputStream obs = new ObjectOutputStream(out);
+
+            obs.writeObject(obj);
+            obs.close();
+
+            //分配内存，写入原始对象，生成新对象
+            ByteArrayInputStream ins = new ByteArrayInputStream(out.toByteArray());
+            ObjectInputStream ois = new ObjectInputStream(ins);
+            //返回生成新对象
+            cloneObj = (T) ois.readObject();
+            ois.close();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return cloneObj;
+    }
+}
